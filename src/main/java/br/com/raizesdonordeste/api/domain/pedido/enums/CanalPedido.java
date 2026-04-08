d5efe14df8002e9e7f8cc82ece219286a584dca2
Douglas Sacramento <douglasj.sacramento@gmail.com>
2026-04-08 12:48:48 -0300

    feat: define valores dos ENUMS CanalPedido e StatusPedido

--- src/main/java/br/com/raizesdonordeste/api/domain/pedido/enums/CanalPedido.java
+++ src/main/java/br/com/raizesdonordeste/api/domain/pedido/enums/CanalPedido.java
@@ -1,4 +1,9 @@
 package br.com.raizesdonordeste.api.domain.pedido.enums;
 
 public enum CanalPedido {
+    APP,
+    TOTEM,
+    BALCAO,
+    PICKUP,
+    WEB
 }
--- src/main/java/br/com/raizesdonordeste/api/domain/pedido/enums/StatusPedido.java
+++ src/main/java/br/com/raizesdonordeste/api/domain/pedido/enums/StatusPedido.java
@@ -1,4 +1,9 @@
 package br.com.raizesdonordeste.api.domain.pedido.enums;
 
 public enum StatusPedido {
+    RECEBIDO,
+    COZINHA,
+    PRONTO,
+    ENTREGUE,
+    CANCELADO
 }
