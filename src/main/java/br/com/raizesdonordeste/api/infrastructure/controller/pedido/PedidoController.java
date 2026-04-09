120bedeafec312ca6e7dee7d266813a35c0fef92
Douglas Sacramento <douglasj.sacramento@gmail.com>
2026-04-08 23:13:03 -0300

    feat(pedido): adiciona endpoint de criação de pedido

---
+++ src/main/java/br/com/raizesdonordeste/api/infrastructure/controller/pedido/PedidoController.java
@@ -0,0 +1,31 @@
+package br.com.raizesdonordeste.api.infrastructure.controller.pedido;
+
+import br.com.raizesdonordeste.api.application.dto.PedidoRequestDTO;
+import br.com.raizesdonordeste.api.application.dto.PedidoResponseDTO;
+import br.com.raizesdonordeste.api.application.service.PedidoService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/pedidos")
+public class PedidoController {
+
+    private final PedidoService pedidoService;
+
+    @PostMapping
+    public ResponseEntity<PedidoResponseDTO> criar(@Valid @RequestBody PedidoRequestDTO dados, UriComponentsBuilder uriBuilder){
+        var pedido = pedidoService.criarPedido(dados);
+        URI uri = uriBuilder.path("/api/v1/pedidos/{id}").buildAndExpand(pedido.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new PedidoResponseDTO(pedido));
+    }
+}
