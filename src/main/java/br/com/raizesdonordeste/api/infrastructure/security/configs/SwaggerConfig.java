755af59f3b3ad42ef60afdd8d3156f569a24c16c
Douglas Sacramento <douglasj.sacramento@gmail.com>
2026-04-16 17:37:12 -0300

    feat(api): adiciona a documentação via Swagger e suas congigurações na API

--- src/main/java/br/com/raizesdonordeste/api/infrastructure/security/configs/SwaggerConfig.java
+++ src/main/java/br/com/raizesdonordeste/api/infrastructure/security/configs/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package br.com.raizesdonordeste.api.infrastructure.config;
+package br.com.raizesdonordeste.api.infrastructure.security.configs;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
