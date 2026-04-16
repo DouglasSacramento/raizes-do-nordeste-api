f91e73da0f24f28256bddafd0cecfda887be4976
Douglas Sacramento <douglasj.sacramento@gmail.com>
2026-04-16 17:34:59 -0300

    --amend

---
+++ src/main/resources/db/migration/V2__insert_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO unidades (nome, telefone)
+VALUES ('Raízes do Nordeste - Matriz', '81999999999');
+
+INSERT INTO usuarios (login, senha, role, unidade_id)
+VALUES ('admin@raizesdonordeste.com', '$2a$10$pxMKIAHHbyIA53Yw.0.03ep0VBnsEozKbODUx25hun2hNX9D73wQ.', 'ADMIN', 1);
