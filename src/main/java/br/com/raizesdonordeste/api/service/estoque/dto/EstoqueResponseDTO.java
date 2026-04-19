2bfa1e71d4ae850e28c485f9d6f21e0b545f783a
Douglas Sacramento <douglasj.sacramento@gmail.com>
2026-04-19 10:01:52 -0300

    feat(estoque): adiciona os nomes da unidade e do produto ao EstoqueResponseDTO

--- src/main/java/br/com/raizesdonordeste/api/service/estoque/dto/EstoqueResponseDTO.java
+++ src/main/java/br/com/raizesdonordeste/api/service/estoque/dto/EstoqueResponseDTO.java
@@ -3,9 +3,11 @@ package br.com.raizesdonordeste.api.service.estoque.dto;
 import br.com.raizesdonordeste.api.domain.unidade.ItemEstoque;
 
 public record EstoqueResponseDTO(
+        String unidade,
+        String produto,
         Integer estoqueAtualizado
 ) {
     public EstoqueResponseDTO(ItemEstoque itemEstoque) {
-        this(itemEstoque.getQuantidade());
+        this(itemEstoque.getUnidade().getNome(), itemEstoque.getProduto().getNome(), itemEstoque.getQuantidade());
     }
 }
