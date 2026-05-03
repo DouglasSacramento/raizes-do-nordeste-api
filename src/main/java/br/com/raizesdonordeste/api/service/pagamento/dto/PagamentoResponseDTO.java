93c66a4956718d6b651fe324684f2ee5e0682c0f
Douglas Sacramento <douglasj.sacramento@gmail.com>
2026-05-03 17:29:04 -0300

    feat(api): implementa os testes isolados no arquivo Raizes_do_Nordeste_Postman_Collection.json e retorna no PagamentoResponseDTO o statusPedido

--- src/main/java/br/com/raizesdonordeste/api/service/pagamento/dto/PagamentoResponseDTO.java
+++ src/main/java/br/com/raizesdonordeste/api/service/pagamento/dto/PagamentoResponseDTO.java
@@ -2,13 +2,15 @@ package br.com.raizesdonordeste.api.service.pagamento.dto;
 
 import br.com.raizesdonordeste.api.domain.pagamento.Pagamento;
 import br.com.raizesdonordeste.api.domain.pagamento.enums.StatusPagamento;
+import br.com.raizesdonordeste.api.domain.pedido.enums.StatusPedido;
 
 public record PagamentoResponseDTO(
         Long pedidoId,
         String codigoGateway,
-        StatusPagamento statusPagamento
+        StatusPagamento statusPagamento,
+        StatusPedido statusPedido
 ) {
     public PagamentoResponseDTO(Pagamento pagamento) {
-        this(pagamento.getPedido().getId(), pagamento.getCodigoGateway(), pagamento.getStatusPagamento());
+        this(pagamento.getPedido().getId(), pagamento.getCodigoGateway(), pagamento.getStatusPagamento(), pagamento.getPedido().getStatusPedido());
     }
 }
